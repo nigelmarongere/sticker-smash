793e583442181d22e79876a40c3cfa5cf2f543cb
nigelmarongere <nigelmarongere@gmail.com>
2025-02-16 17:25:32 +0000

    updated copy

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function AboutScreen() {
     return(
         <View style={styles.container}>
-            <Text style={styles.text}>About Screen</Text>
+            <Text style={styles.text}>About</Text>
         </View>
     );
 };
